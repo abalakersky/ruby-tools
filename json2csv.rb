c803ce3fa446d11454e3529a04a56704a7fee230
Alex Balakersky <alex@balakersky.com>
2017-01-05 10:36:09 -0500

    Testing

--- json2csv.rb
+++ json2csv.rb
@@ -7,3 +7,5 @@ json_converter = JsonConverter.new
 json = File.open("/home/abalakersky/Downloads/tmp/instances.json")
 csv = json_converter.generate_csv json
 json_converter.write_to_csv json, 'output.csv'
+
+puts json
